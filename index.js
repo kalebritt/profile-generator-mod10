dcc347f89b3a431b7a43a8f1c63f75ee33dae81b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-08-28 13:03:03 -0600

    added createTeam function index.js

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ const Manager = require("./employees/intern");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
+teamArray = [];
+
+
+
+function runApp () {
+
+  function createTeam () {
+    inquirer.prompt([{
+      type: "list",
+      message: "What type of employee would you like to add to your team?",
+      name: "addEmployeePrompt",
+      choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+    }]).then(function (userInput) {
+      switch(userInput.addEmployeePrompt) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+
+        default:
+          htmlBuilder();
+      }
+    })
+  }};
 
 
 //manager? prompt
