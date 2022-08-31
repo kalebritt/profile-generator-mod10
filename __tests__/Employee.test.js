ba3f981ea23c22a59d96218cda94bcf532bd995e
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-08-31 16:39:30 -0600

    working on employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,6 @@
-const Employee = require('../lib/Employee')
+const Employee = require("../lib/Employee");
 
-
-
+test("Employee? We shall see.", () => {
+  const teamMember = new Employee();
+  expect(typeof teamMember).tobe("the thing");
+});
