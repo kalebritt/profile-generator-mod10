442f5d2c4e8cbbd06fced519a976d931cb49f45a
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-08-31 16:59:04 -0600

    more testing functions in employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,3 +4,15 @@ test("Employee? We shall see.", () => {
   const teamMember = new Employee();
   expect(typeof teamMember).tobe("the thing");
 });
+
+test('Name test, if you"ve got one.', () => {
+    const teamMember = new Employee(name);
+    const name = 'name';
+    expect(teamMember.name).tobe(name) 
+});
+
+test('ID? Show me.', () => {
+    const teamMember = new Employee('name', id);
+    const id = id;
+    expect(teamMember.id).tobe(id) 
+});
